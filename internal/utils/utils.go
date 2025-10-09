72f74a3f8da3f36a5ff27ef9c93d8569c690547b
takashiraki <stgtbike4649@gmail.com>
2025-10-09 21:26:11 +0900

    add: create env method.

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,7 +1,11 @@
 package utils
 
 import (
+	"errors"
 	"fmt"
+	"io"
+	"os"
+	"path/filepath"
 	"time"
 )
 
@@ -28,6 +32,43 @@ func ShowLoadingIndicator(message string, done chan bool) {
 func checkDir(dir string) error {
 	if DirIsExists(dir) {
 		return fmt.Errorf("directory %s already exists", dir)
+	}
+
+	return nil
+}
+
+func CreateEnvFile(projectPath string) error {
+	envExampleFilePath := filepath.Join(projectPath, ".env.example")
+
+	if _, err := os.Stat(envExampleFilePath); os.IsNotExist(err) {
+		return errors.New(".env.example file does not exist")
+	}
+
+	envFilePath := filepath.Join(projectPath, ".env")
+
+	if _, err := os.Stat(envFilePath); !os.IsNotExist(err) {
+		fmt.Println(".env file already exists, skipping creation.")
+		return nil
+	}
+
+	src, err := os.Open(envExampleFilePath)
+
+	if err != nil {
+		return errors.New("error opening .env.example file: " + err.Error())
+	}
+
+	defer src.Close()
+
+	dst, err := os.Create(envFilePath)
+
+	if err != nil {
+		return errors.New("error creating .env file: " + err.Error())
+	}
+
+	defer dst.Close()
+
+	if _, err := io.Copy(dst, src); err != nil {
+		return errors.New("error copying .env.example to .env: " + err.Error())
 	}
 
 	return nil
