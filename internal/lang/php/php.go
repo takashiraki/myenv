bde5b3acf662c4ac54f7e83be74ff87adec3bea8
tks <87138608+takashiraki@users.noreply.github.com>
2025-10-16 09:16:45 +0900

    clone PHP (#12)
    * add: validate php projects.
    
    * add: include validate in clone php.
    
    * add: make it common about extract repo name.
    
    * add: confirm clone php.
    
    * fix: confirm view
    
    * add: git clone command.
    
    * add: repo project validate.
    
    * fix: use common clone command.
    
    * add: create env method.
    
    * add: use create env method.
    
    * add: create env file in clone php usecase.
    
    * add: create docker container.

--- internal/lang/php/php.go
+++ internal/lang/php/php.go
@@ -2,6 +2,7 @@ package php
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"myenv/internal/config"
 	"myenv/internal/utils"
@@ -44,6 +45,7 @@ func PHP() {
 	switch clone {
 	case "Yes":
 		fmt.Println("Clone project")
+		cloneProject()
 	case "No":
 		fmt.Println("Create project")
 		createProject()
@@ -324,5 +326,300 @@ func createConfigFile(containerName string, containerPort int, path string, lang
 
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func cloneProject() {
+	gitRepo := ""
+	gitRepoPrompt := &survey.Input{
+		Message: "Enter the Git repository URL of PHP project : ",
+	}
+
+	err := survey.AskOne(
+		gitRepoPrompt, &gitRepo,
+		survey.WithValidator(survey.Required),
+		survey.WithValidator(utils.ValidateGitRepoUrl),
+		survey.WithValidator(utils.ValidateGitRepoProjectExists),
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	containerPort := 0
+	containerPortPrompt := &survey.Input{
+		Message: "Enter the port of PHP : ",
+	}
+
+	portErr := survey.AskOne(
+		containerPortPrompt, &containerPort,
+		survey.WithValidator(survey.Required),
+		survey.WithValidator(utils.ValidatePort),
+	)
+
+	if portErr != nil {
+		log.Fatal(portErr)
+	}
+
+	utils.ClearTerminal()
+
+	repoName := utils.ExtractionRepoName(gitRepo)
+
+	homeDir, err := os.UserHomeDir()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	path := filepath.Join(homeDir, "dev", repoName)
+	containerName := repoName
+
+	fmt.Print(`
+ _____ _      ___  _   _ _____   ____  _   _ ____
+/ ____| |    / _ \| \ | | ____| |  _ \| | | |  _ \
+| |   | |   | | | |  \| |  _|   | |_) | |_| | |_) |
+| |___| |___| |_| | |\  | |___  |  __/|  _  |  __/
+\_____|_____|\___/|_| \_|_____| |_|   |_| |_|_|
+
+`)
+
+	fmt.Println("╔═════════════════════════════════════════════════════╗")
+	fmt.Println("║                 Configuration                       ║")
+	fmt.Println("╠═════════════════════════════════════════════════════╣")
+	fmt.Printf("║ Container name : %-34s ║\n", containerName)
+	fmt.Printf("║ Clone path     : %-34s ║\n", path)
+	fmt.Printf("║ Port           : %-34d ║\n", containerPort)
+	fmt.Println("║ Framework      : None                               ║")
+	fmt.Println("║ Language       : PHP                                ║")
+	fmt.Println("╚═════════════════════════════════════════════════════╝")
+
+	var confirmResult bool
+
+	confirmPrompt := &survey.Confirm{
+		Message: "Is it okay to start building the environment with this configuration?",
+	}
+
+	if err := survey.AskOne(confirmPrompt, &confirmResult); err != nil {
+		log.Fatal(err)
+	}
+
+	if !confirmResult {
+		fmt.Println("Please try again")
+		cloneProject()
+	}
+
+	fw := "none"
+	lang := "php"
+	options := map[string]string{
+		"type": "clone",
+		"repo": gitRepo,
+	}
+
+	createConfigFile(
+		containerName,
+		containerPort,
+		path,
+		lang,
+		fw,
+		options,
+	)
+
+	targetRepo := "https://github.com/takashiraki/docker_php.git"
+
+	done := make(chan bool)
+
+	go utils.ShowLoadingIndicator("Cloning repository", done)
+
+	if err := utils.CloneRepo(targetRepo, path); err != nil {
+		done <- true
+		log.Fatalf(`\r\033[Error!!\n%v`, err)
+	}
+
+	done <- true
+
+	fmt.Printf("\r\033[KCloning repository completed ✓\n")
+
+	done = make(chan bool)
+
+	go utils.ShowLoadingIndicator("Creating .env file", done)
+
+	if err := utils.CreateEnvFile(path); err != nil {
+		done <- true
+		log.Fatalf("\r\033[Kerror creating .env file: %v", err)
+	}
+
+	done <- true
+
+	fmt.Printf("\r\033[KCreating .env file completed ✓\n")
+
+	done = make(chan bool)
+
+	go utils.ShowLoadingIndicator("Clone your PHP project.", done)
+
+	srcTargetPath := filepath.Join(path, "src", containerName)
+
+	if err := utils.CloneRepo(gitRepo, srcTargetPath); err != nil {
+		done <- true
+		log.Fatalf(`\r\033[Error!!\n%v`, err)
+	}
+
+	done <- true
+
+	fmt.Printf("\r\033[KCloning your PHP project completed ✓\n")
+
+	done = make(chan bool)
+
+	go utils.ShowLoadingIndicator("Setup env file", done)
+
+	envFilePath := filepath.Join(path, ".env")
+
+	repositoryPath := fmt.Sprintf("src/%s", containerName)
+	hostPort := 80
+
+	content, err := os.ReadFile(envFilePath)
+
+	if err != nil {
+		done <- true
+		log.Fatalf("\r\033[Kerror reading .env file: %v", err)
+	}
+
+	updateContent := string(content)
+
+	updateContent = strings.ReplaceAll(updateContent, "REPOSITORY_PATH=", fmt.Sprintf("REPOSITORY_PATH=%s", repositoryPath))
+	updateContent = strings.ReplaceAll(updateContent, "CONTAINER_NAME=", fmt.Sprintf("CONTAINER_NAME=%s", containerName))
+	updateContent = strings.ReplaceAll(updateContent, "HOST_PORT=", fmt.Sprintf("HOST_PORT=%d", containerPort))
+	updateContent = strings.ReplaceAll(updateContent, "CONTAINER_PORT=", fmt.Sprintf("CONTAINER_PORT=%d", hostPort))
+
+	if err := os.WriteFile(envFilePath, []byte(updateContent), 0644); err != nil {
+		done <- true
+		log.Fatalf("\r\033[Kerror writing .env file: %v", err)
+	}
+
+	done <- true
+
+	fmt.Printf("\r\033[KSetup .env file completed ✓\n")
+
+	done = make(chan bool)
+
+	go utils.ShowLoadingIndicator("Creating container workspace", done)
+
+	devContainerPath := filepath.Join(path, ".devcontainer", "devcontainer.json")
+
+	devContainerExamplePath := filepath.Join(path, ".devcontainer", "devcontainer.json.example")
+
+	if _, err := os.Stat(devContainerExamplePath); os.IsNotExist(err) {
+		done <- true
+		log.Fatalf("\r\033[Kerror: .devcontainer.json.example file does not exist in the repository")
+	}
+
+	if _, err := os.Stat(devContainerPath); err == nil {
+		done <- true
+		log.Fatalf("\r\033[Kerror: .devcontainer.json file already exists")
+	} else {
+		src, err := os.Open(devContainerExamplePath)
+
+		if err != nil {
+			done <- true
+			log.Fatalf("\r\033[Kerror opening .devcontainer.json.example file: %v", err)
+		}
+
+		defer src.Close()
+
+		dest, err := os.Create(devContainerPath)
+
+		if err != nil {
+			done <- true
+			log.Fatalf("\r\033[Kerror creating .devcontainer.json file: %v", err)
+		}
+
+		defer dest.Close()
+
+		if _, err := io.Copy(dest, src); err != nil {
+			done <- true
+			log.Fatalf("\r\033[Kerror copying .devcontainer.json.example to .devcontainer.json: %v", err)
+		}
+
+		devContainerContents, err := os.ReadFile(devContainerPath)
+
+		if err != nil {
+			done <- true
+			log.Fatalf("\r\033[Kerror reading .devcontainer.json file: %v", err)
+		}
+
+		updateDevContainerContents := string(devContainerContents)
+
+		updateDevContainerContents = strings.ReplaceAll(updateDevContainerContents, `"name": "php debug",`, fmt.Sprintf(`"name": "%s",`, containerName))
+
+		if err := os.WriteFile(devContainerPath, []byte(updateDevContainerContents), 0644); err != nil {
+			done <- true
+			log.Fatalf("\r\033[Kerror writing .devcontainer.json file: %v", err)
+		}
+	}
+
+	done <- true
+
+	fmt.Printf("\r\033[KCreating container workspace completed ✓\n")
+
+	done = make(chan bool)
+
+	go utils.ShowLoadingIndicator("Starting Docker containers", done)
+
+	dockerCmd := exec.Command("docker", "compose", "up", "-d", "--build")
+
+	dockerCmd.Dir = path
+
+	if _, err := dockerCmd.CombinedOutput(); err != nil {
+		done <- true
+		log.Fatalf("\r\033[Kerror starting Docker containers: %v", err)
+	}
+
+	done <- true
+	fmt.Printf("\r\033[KStarting Docker containers completed ✓\n")
+
+	utils.ClearTerminal()
+
+	fmt.Print(`
+  ____  ___  __  __ ____  _     _____ _____ _____      /\   /\
+ / ___|/ _ \|  \/  |  _ \| |   | ____|_   _| ____|    (  ._. )
+| |   | | | | |\/| | |_) | |   |  _|   | | |  _|       > ^ <
+| |___| |_| | |  | |  __/| |___| |___  | | | |___     /     \
+ \____|\___/|_|  |_|_|   |_____|_____| |_| |_____|   /_______\
+
+`)
+
+	fmt.Println("╔══════════════════════════════════════════════════════════════════╗")
+	fmt.Println("║                   🎉 SETUP COMPLETE! 🎉                          ║")
+	fmt.Println("╠══════════════════════════════════════════════════════════════════╣")
+	fmt.Printf("║ 📦 Container Name : %-44s ║\n", containerName)
+	fmt.Printf("║ 📂 Repository Path: %-44s ║\n", path)
+	fmt.Printf("║ 🌐 Port          : %-45d ║\n", containerPort)
+	fmt.Println("╠══════════════════════════════════════════════════════════════════╣")
+	fmt.Println("║                          Next Steps:                             ║")
+	fmt.Printf("║  • Open VS Code: code %-42s ║\n", path)
+	fmt.Printf("║  • Access app  : http://localhost:%-30d ║\n", containerPort)
+	fmt.Println("║  • Start coding in the devcontainer! 🚀                          ║")
+	fmt.Println("╚══════════════════════════════════════════════════════════════════╝")
+
+	codeVersionCommand := exec.Command("code", "--version")
+
+	if _, err = codeVersionCommand.CombinedOutput(); err == nil {
+
+		var openInVSCode bool
+
+		openInVSCodePrompt := &survey.Confirm{
+			Message: "Do you want to open the project in VS Code?",
+		}
+
+		if err := survey.AskOne(openInVSCodePrompt, &openInVSCode); err != nil {
+			log.Fatal(err)
+		}
+
+		if openInVSCode {
+			openCommand := exec.Command("code", path)
+
+			if _, err := openCommand.CombinedOutput(); err != nil {
+				log.Fatalf("error opening project in VS Code: %v", err)
+			}
+		}
 	}
 }
