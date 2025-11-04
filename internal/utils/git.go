92e5e6987c0af4adf5ece054006150079228ad3a
takashiraki <stgtbike4649@gmail.com>
2025-11-04 11:37:21 +0900

    fix: create php error handling.

--- internal/utils/git.go
+++ internal/utils/git.go
@@ -1,27 +1,3 @@
 package utils
 
-import (
-	"fmt"
-	"myenv/internal/infrastructure"
-)
 
-func CloneRepoHandle(
-	p infrastructure.RepositoryInterface,
-	repoUrl string,
-	path string,
-) error {
-	done := make(chan bool)
-
-	go ShowLoadingIndicator("Cloning repository", done)
-
-	if err := p.CloneRepo(repoUrl, path); err != nil {
-		done <- true
-
-		fmt.Printf("\r\033[31m✗ Error:\033[0m Failed to clone repository\n")
-
-		errMsg := err.Error()
-	}
-
-	done <- true
-	return nil
-}
