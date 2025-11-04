91ed2a2d662dc58b51a88b54f3f41000c1dcd4c5
takashiraki <stgtbike4649@gmail.com>
2025-11-04 17:47:33 +0900

    add: version.

--- cmd/root.go
+++ cmd/root.go
@@ -11,7 +11,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var version = "v0.2.5"
+var version = "v0.3.0"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
