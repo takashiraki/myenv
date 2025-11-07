67a2b85f5c65dcf97ebdc711c6906a8ab9573edd
takashiraki <stgtbike4649@gmail.com>
2025-11-07 13:26:26 +0900

    fix: config service.

--- internal/config/application/config_service.go
+++ internal/config/application/config_service.go
@@ -2,14 +2,16 @@ package application
 
 import (
 	"encoding/json"
+	"errors"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type (
 	Config struct {
-		lang             string
-		containerRuntime string
+		Lang             string
+		ContainerRuntime string
 	}
 
 	ConfigService struct {
@@ -26,16 +28,21 @@ func NewConfigService() (*ConfigService, error) {
 
 	path := filepath.Join(homedir, ".config", "myenv", "config.json")
 
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		return nil, err
-	}
-
 	return &ConfigService{
 		path: path,
 	}, nil
 }
 
 func (s *ConfigService) GetConfig() (Config, error) {
+	if _, err := os.Stat(s.path); os.IsNotExist(err) {
+		errMsg := err.Error()
+
+		switch {
+			case strings.Contains(errMsg, "no such file or directory"):
+				return Config{}, errors.New("config file does not exist")
+		}
+	}
+
 	data, err := os.ReadFile(s.path)
 
 	if err != nil {
@@ -49,4 +56,36 @@ func (s *ConfigService) GetConfig() (Config, error) {
 	}
 
 	return config, nil
+}
+
+func (s *ConfigService) CreateConfig(
+	lang string,
+	containerRuntime string,
+) error {
+	//If the config file already exists, return nil.
+	if _, err := os.Stat(s.path); err == nil {
+		return  nil		
+	}
+
+	dir := filepath.Dir(s.path)
+
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
+
+	config := Config{
+		Lang: lang,
+		ContainerRuntime: containerRuntime,
+	};
+
+	data, err := json.MarshalIndent(config, "", "  ")
+
+	if err != nil {
+		return  err
+	}
+	if err := os.WriteFile(s.path, data, 0644); err != nil {
+		return err
+	}
+
+	return nil
 }
