c51a4e6be22f1d582383adc8a043705deb7a8030
takashiraki <stgtbike4649@gmail.com>
2025-11-10 17:40:00 +0900

    add: methods of network exist.

--- internal/infrastructure/container.go
+++ internal/infrastructure/container.go
@@ -3,4 +3,7 @@ package infrastructure
 type ContainerInterface interface {
 	CreateContainer(path string) error
 	ChechProxyNetworkExists() error
+	ChechInfraNetworkExists() error
+	CreateProxyNetwork() error
+	CreateInfraNetwork() error
 }
--- internal/infrastructure/docker_container.go
+++ internal/infrastructure/docker_container.go
@@ -39,3 +39,43 @@ func (d *DockerContainer) ChechProxyNetworkExists() error {
 
 	return nil
 }
+
+func (d *DockerContainer) ChechInfraNetworkExists() error {
+	cmd := exec.Command("docker", "network", "ls", "--filter", "name=my_infra_network")
+
+	output, err := cmd.CombinedOutput()
+
+	if err != nil {
+		return errors.New("Error running docker network ls --filter name=my_infra_network: " + err.Error() + ", output: " + string(output))
+	}
+
+	if !strings.Contains(string(output), "my_infra_network") {
+		return errors.New("my_infra_network does not exist")
+	}
+
+	return nil
+}
+
+func (d *DockerContainer) CreateProxyNetwork() error {
+	cmd := exec.Command("docker", "network", "create", "my_proxy_network")
+
+	output, err := cmd.CombinedOutput()
+
+	if err != nil {
+		return errors.New("Error running docker network create my_proxy_network: " + err.Error() + ", output: " + string(output))
+	}
+
+	return nil
+}
+
+func (d *DockerContainer) CreateInfraNetwork() error {
+	cmd := exec.Command("docker", "network", "create", "my_infra_network")
+
+	output, err := cmd.CombinedOutput()
+
+	if err != nil {
+		return errors.New("Error running docker network create my_infra_network: " + err.Error() + ", output: " + string(output))
+	}
+
+	return nil
+}
