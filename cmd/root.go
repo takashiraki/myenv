9d7786e78c8f153cfc788288d10b9d0405952873
takashiraki <stgtbike4649@gmail.com>
2025-11-13 15:19:19 +0900

    fix: typo.

--- cmd/root.go
+++ cmd/root.go
@@ -9,7 +9,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var version = "v0.6.0"
+var version = "v0.6.1"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
