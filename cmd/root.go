4156d1c9a1426c33eac1369df47d43100ff48bfb
takashiraki <stgtbike4649@gmail.com>
2025-11-23 12:32:18 +0900

    fix: version.

--- cmd/root.go
+++ cmd/root.go
@@ -9,7 +9,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var version = "v0.7.2"
+var version = "v0.8.0"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
